bf6afc5a893acc60af6b56c83e9e35fe3d680468
AsijitM <asijit00@gmail.com>
2024-02-15 00:51:34 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -13,8 +13,13 @@ app.use(
 app.use(express.json({ limit: "20kb" }));
 // allows express to read encoded urls such as '%20'-- hello%20world
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// Routes import
+import userRouter from "./routes/user.routes.js";
+
+// Routes Declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
