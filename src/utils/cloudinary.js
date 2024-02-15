4b6558c1511b4a54db68e08e2504cc3f95272e59
AsijitM <asijit00@gmail.com>
2024-02-15 09:53:06 +0530

    user controller image

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,7 +5,6 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
 });
 
 export const uploadOnCloudinary = async (localFilePath) => {
@@ -15,8 +14,10 @@ export const uploadOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
+    // console.log(response);
     //   file has been uploaded successfully
-    console.log("File is uploaded on cloudinary !!", response.url);
+    // console.log("File is uploaded on cloudinary !!", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation gets failed
